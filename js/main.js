038323f5f79b595cf43920c65466523191b7da6a
ChowdhuryRafique <chowdhuryrafiq511@gmail.com>
2019-07-28 14:31:35 +0600

    This is for the first addintion and next is update the products and user_profile

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+    
+    $('.dropdown-toggle').dropdown();
+});
+Morris.Area({
+    element: 'graph',
+    behaveLikeLine: true,
+    data: [
+        {x: '2011 Q1', y: 3, z: 3},
+        {x: '2011 Q2', y: 2, z: 1},
+        {x: '2011 Q3', y: 2, z: 4},
+        {x: '2011 Q4', y: 3, z: 3}
+    ],
+    xkey: 'x',
+    ykeys: ['y', 'z'],
+    labels: ['Y', 'Z']
+});
